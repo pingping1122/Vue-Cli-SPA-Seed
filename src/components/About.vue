5ca8e339870ebeab69a7a8e3ef3ed1e2da937405
pingping100 <2652902589@qq.com>
2019-05-22 10:05:40 +0800

    利用vue-cli实现SPA的种子应用

---
+++ src/components/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <h1> hi , 欢迎来到 About 页面！</h1>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "About"
+    }
+</script>
+
+<style scoped>
+
+</style>
